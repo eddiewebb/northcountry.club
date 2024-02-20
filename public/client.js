750865727709cb6ddc7e67db02727e322315f620
Eddie Webbinaro <ollitech@gmail.com>
2024-02-19 20:48:39 -0500

    radar

--- public/client.js
+++ public/client.js
@@ -6,6 +6,7 @@
 
 const factionPieEl = document.getElementById('factionPie');
 const playerPieEl = document.getElementById('playerPie');
+const combinedRadarEl = document.getElementById('combinedRadar');
 const gamesTableEl = document.getElementById('gamesTable');
 
 Chart.register(ChartDataLabels);
@@ -30,21 +31,23 @@ const tableColumns = [
   'Winning Player',
   'Low Score'
 ]
-
+const players = ['Caleb','Eli','Eddie','Rubin']
 
 
 // Appends the blocks API response to the UI
 const updateChart = function (apiResponse) {
-  console.log(apiResponse)
-  const factionWins = {  };
-  const playerWins = {  };
+  const factionWins = labels.reduce((full,current)=>(full[current]=0,full),{})
+  const playerWins = players.reduce((full,current)=>(full[current]=0,full),{})
+  const playerFactionWins = players.reduce((full,current)=>(full[current]=labels.reduce((full,current)=>(full[current]=0,full),{}),full),{})
+  console.log(playerFactionWins);
   apiResponse['data']['results'].map( async (row,index)=>{
     const faction = row.properties['Winning Faction'].formula.string;
     const player = row.properties['Winning Player'].formula.string;
-    factionWins[faction] ? factionWins[faction]++ : factionWins[faction] = 1
-    playerWins[player] ? playerWins[player]++ : playerWins[player] = 1
+    factionWins[faction]++ 
+    playerWins[player]++ 
+    playerFactionWins[player][faction]++ 
   });
-  console.log(factionWins);
+  console.log(playerFactionWins);
   const data = {
     labels: labels,
       datasets: [{        
@@ -56,10 +59,9 @@ const updateChart = function (apiResponse) {
           labels: {
             name: {
               align: 'top',
-              font: {size: 22, weight: 'bold'},
+              font: {size: '14rem', weight: 'bold'},
               color: 'white',
               formatter: function(value, ctx) {
-                console.log(ctx)
                 return ctx.active
                   ? 'name'
                   : ctx.chart.data.labels[ctx.dataIndex];
@@ -80,22 +82,28 @@ const updateChart = function (apiResponse) {
     };
   new Chart(factionPieEl, {
       type: 'pie',
+      options: {
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+      },
       data: data
     });
   const data2 = {
-      labels: ['Caleb','Eli','Eddie','Rubin'],
+      labels: players,
         datasets: [{        
           label: 'My First Dataset',
-          data:  ['Caleb','Eli','Eddie','Rubin'].map((key) => playerWins[key]),
+          data: players.map((key) => playerWins[key]),
           hoverOffset: 4,
           datalabels: {
             labels: {
               name: {
                 align: 'top',
-                font: {size: 22, weight: 'bold'},
+                font: {size: '14em', weight: 'bold'},
                 color: 'white',
                 formatter: function(value, ctx) {
-                  console.log(ctx)
                   return ctx.active
                     ? 'name'
                     : ctx.chart.data.labels[ctx.dataIndex];
@@ -116,8 +124,45 @@ const updateChart = function (apiResponse) {
   new Chart(playerPieEl, {
       type: 'pie',
       data:  data2,
-  
+      options: {
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+      },
     });
+
+  const data3 = {
+    labels: labels,
+      datasets: 
+        players.map((player)=>{
+          return new Object({
+            label: player,
+            data:  labels.map((key) => playerFactionWins[player][key]),
+          })
+        })       
+    };
+new Chart(combinedRadarEl, {
+    type: 'radar',
+    data:  data3,
+    options: {
+      plugins: {
+          legend: {
+              display: false
+          }
+      },scales: {
+        r: {
+            min: -.5,
+            max: 4,
+            ticks: {
+              stepSize: 1
+            }
+        }
+    }
+    },
+
+  });
 }
 
 const updateTable = function (apiResponse) {
